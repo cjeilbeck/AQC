0dc597f1d0ce4435cd2bc43bc343fcce23a98084
Cameron Eilbeck <cameroneilbeck@outlook.com>
2024-11-12 11:22:28 +0000

    AQC SIMPLE V1.1

--- EVOLUTION.py
+++ EVOLUTION.py
@@ -1,4 +1,4 @@
-#defining starting state of phi
+
 from PAULI_DEF import pauli, X, Y, Z, I
 from H_ising import ising_hamiltonian
 from h_vector import h_mat
@@ -6,16 +6,11 @@ import numpy as np
 import scipy as sp
 from scipy.linalg import expm
 
-
-
-# defining A,B time variables 
-
 def A(tmax, t):
     return 1-(t/tmax)
 def B(tmax, t):
     return t/tmax
 
-# defining Time Dependent Hamiltonian
 
 def Hamiltonian(t, tmax, Ising, n):
     H = np.zeros((2**n, 2**n), dtype=complex)
@@ -23,7 +18,6 @@ def Hamiltonian(t, tmax, Ising, n):
         H += pauli(n,i,X)
     return -(A(tmax,t)* H) + (B(tmax,t)*Ising)
 
-#defining evolution
 
 def Evolution(tmax, Ising, n, q, k, phi0, phi_target):
     tau = tmax / q
@@ -31,14 +25,11 @@ def Evolution(tmax, Ising, n, q, k, phi0, phi_target):
     fidelities = []
 
     for i in range(1, k + 1):
-        # Calculate time-dependent Hamiltonian at time t = i * tau
         H_t = Hamiltonian(i * tau, tmax, Ising, n)
         exp_term = expm(-1j * tau * H_t)
 
-        # Apply in time-ordered manner (new term on the left)
         product = np.matmul(exp_term, product)
 
-        # Calculate evolved state and fidelity
         evolved_state = np.matmul(product, phi0)
         fidelity = np.abs(np.inner(evolved_state, phi_target))**2
         fidelities.append(fidelity)
